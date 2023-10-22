920253577506c8a53216f60598251991328daeb6
Hill Onyechekwa <hillonyechekwa@gmail.com>
2023-10-22 23:12:56 +0100

    Revert "second commit"

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig){
 	eleventyConfig.addPassthroughCopy('./src/assets')
 	eleventyConfig.addPassthroughCopy('./src/favicon')
 	eleventyConfig.addPassthroughCopy('./src/scripts')
-	eleventyConfig.addPassthroughCopy('./src/css/')
+	eleventyConfig.addWatchTarget('./src/scss/')
 	return{
 		dir: {
 			input: "src",
