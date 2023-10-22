693a0053191d447948ef552bb9aedc54d4853be9
hillary <hillonyechekwa@gmail.com>
2023-10-22 23:31:36 +0100

    readjusted commit

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig){
 	eleventyConfig.addPassthroughCopy('./src/assets')
 	eleventyConfig.addPassthroughCopy('./src/favicon')
 	eleventyConfig.addPassthroughCopy('./src/scripts')
-	eleventyConfig.addWatchTarget('./src/scss/')
+	eleventyConfig.addPassthroughCopy('./src/css/')
 	return{
 		dir: {
 			input: "src",
